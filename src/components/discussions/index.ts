8ee09868dab83d001d4eb9c1d08531499ca92761
cmnwankw-cmu-F24 <cmnwankw@andrew.cmu.edu>
2025-07-24 10:52:59 +0200

    removed the unused replymodal

--- src/components/discussions/index.ts
+++ src/components/discussions/index.ts
@@ -1,5 +1,4 @@
 export { DiscussionCard } from "./DiscussionCard";
 export { CreatePostModal, type CreatePostData } from "./CreatePostModal";
 export { ReportModal } from "./ReportModal";
-export { ReplyModal } from "./ReplyModal";
 export { TagFilter } from "./TagFilter";
